b84dfb7334f1f66dfb98722c2f3be68327e5d19e
Michael-Antonni <72481616+Michael-Antonni@users.noreply.github.com>
2022-03-22 09:49:25 -0500

    conexion a la base de datos

---
+++ src/datebase/libros.js
@@ -0,0 +1,16 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBF8Xl_NrPO4zgWP_lI0kxP881QAhDxJs4",
+    authDomain: "yeremenko-b8385.firebaseapp.com",
+    projectId: "yeremenko-b8385",
+    storageBucket: "yeremenko-b8385.appspot.com",
+    messagingSenderId: "657864554801",
+    appId: "1:657864554801:web:7b1e65f126ff22ecf31810",
+    measurementId: "G-D4DDQR1X76"
+  };
+    // Initialize Firebase
+  
+    firebase.initializeApp(firebaseConfig);
+  
+    const db = firebase.firestore();
+    
